7bf73e497f4e7d1a13c00bacc44b69c76d7c1893
Jesus99eng <savilas@autonoma.edu.pe>
2024-10-02 17:04:17 -0500

    React-ejm

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+## Para ejecutar el proyecto :
+## 1: npm i
+## 2: npm run dev
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div>
+          <p>Dragon Ball Super</p>
+        </div>
+        <div>
+          <a href="savilas@autonoma.edu.pe">savilas@autonoma.edu.pe</a>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
